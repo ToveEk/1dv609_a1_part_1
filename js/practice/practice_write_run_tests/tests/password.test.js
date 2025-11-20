284490cf72babbcd0ac1ba0005b10b0b07543a50
Tove Ek <te222uu@student.lnu.se>
2025-11-20 14:39:13 +0100

    Add tests for valid and too short password

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,7 +1,7 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
+// import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
@@ -11,15 +11,35 @@ import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
 // import { Password } from '../src/BugWrongMessage'
-// import { Password } from '../src/Correct'
+import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
     const emptyPassword = '';
+    const validPassword = 'ValidPassword1'
+    const pwWith12Characters = 'Password1234'
+    const pwWith11Characters = 'Password123'
+    const tooShortPw = 'TooShort1'
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
+
+    // constructor test
+    test('create valid password', () => {
+        expect(() => {
+            new Password(validPassword)
+        }).not.toThrow()
     });
 
-    //Add your tests here
+    // test for exactly 12 characters (edge case)
+    test('valid password with exactly 12 characters', () => {
+        expect(() => {
+            new Password(pwWith12Characters)
+        }).not.toThrow()
+    })
+
+    // test for 11 characters (edge case)
+    test('too short password with exactly 11 characters should trow error', () => {
+        expect(() => {
+            new Password(pwWith11Characters)
+        }).toThrow('Too short password')
+    })
 });
