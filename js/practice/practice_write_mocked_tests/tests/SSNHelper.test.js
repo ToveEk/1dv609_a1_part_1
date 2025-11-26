19bf088cfd08dbebd85c69eca922fae30ba89742
Tove Ek <te222uu@student.lnu.se>
2025-11-26 17:11:16 +0100

    Add tests for incorrect format and messy luhn

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,4 +1,4 @@
- import { SSNHelper } from '../src/correct/SSNHelper'; 
+ import { SSNHelper } from '../src/correct/SSNHelper';
 // import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
 // import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowMonth0"
 // import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat"
@@ -12,11 +12,15 @@ describe('SSNHelper Tests', () => {
     const validDay = 31
     const invalidMonth = 0
     const invalidLength = '950703-12345'
-    
+    const spaceInsteadOfHyphen = '950703 1234'
+    const missingHyphen = '9507031234'
+    const hyphenMisplaced = '95070-31234'
+    const validLuhnCheck = '900101-3029'
+
 
     test('day 31 should pass', () => {
         const result = ssnHelper.isValidDay(validDay)
-        
+
         expect(result).toBe(true)
     })
 
@@ -26,12 +30,28 @@ describe('SSNHelper Tests', () => {
         expect(result).toBe(false)
     })
 
-    test('incorrect format should fail', () => {
-        
+    test('incorrect format (space instead of hyphen) should fail', () => {
+        const result = ssnHelper.isCorrectFormat(spaceInsteadOfHyphen)
+
+        expect(result).toBe(false)
+    })
+
+    test('incorrect format (missing hyphen) should fail', () => {
+        const result = ssnHelper.isCorrectFormat(missingHyphen)
+
+        expect(result).toBe(false)
+    })
+
+    test('incorrect format (hyphen misplaced) should fail', () => {
+        const result = ssnHelper.isCorrectFormat(hyphenMisplaced)
+
+        expect(result).toBe(false)
     })
 
     test('messy luhn should fail', () => {
+        const result = ssnHelper.luhnisCorrect(validLuhnCheck)
 
+        expect(result).toBe(true)
     })
 
     test('wrong length should fail', () => {
