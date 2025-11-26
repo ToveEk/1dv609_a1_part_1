dcc7d4a68b3027ff9b98d68f68ec04577005b12b
Tove Ek <te222uu@student.lnu.se>
2025-11-26 14:37:07 +0100

    Add ssnhelper tests

--- js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
+++ js/practice/practice_write_mocked_tests/tests/SSNHelper.test.js
@@ -1,12 +1,42 @@
-import { SSNHelper } from '../src/correct/SSNHelper'; 
+ import { SSNHelper } from '../src/correct/SSNHelper'; 
+// import { SSNHelper } from '../src/bugs/BuggySSNHelperAllowDayUpTo30';
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperAllowMonth0"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperIncorrectFormat"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperMessyLuhn"
+// import { SSNHelper } from "../src/bugs/BuggySSNHelperWrongLength"
 
 
 
-describe('SSNHelpe Tests', () => {
+describe('SSNHelper Tests', () => {
+    const ssnHelper = new SSNHelper()
+    const validDay = 31
+    const invalidMonth = 0
+    const invalidLength = '950703-12345'
+    
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+    test('day 31 should pass', () => {
+        const result = ssnHelper.isValidDay(validDay)
+        
+        expect(result).toBe(true)
+    })
 
-    //Add your tests here
-});
+    test('month 0 should fail', () => {
+        const result = ssnHelper.isValidMonth(invalidMonth)
+
+        expect(result).toBe(false)
+    })
+
+    test('incorrect format should fail', () => {
+        
+    })
+
+    test('messy luhn should fail', () => {
+
+    })
+
+    test('wrong length should fail', () => {
+        const result = ssnHelper.isCorrectLength(invalidLength)
+
+        expect(result).toBe(false)
+    })
+})
